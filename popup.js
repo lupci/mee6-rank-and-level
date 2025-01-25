fa70ff7ffbb8e4759af32d9b2d6217a9636e617d
lupci <196411549+lupci@users.noreply.github.com>
2025-01-25 04:33:24 -0300

    feat: use with any username, provided that your rank is lower than 100

--- popup.js
+++ popup.js
@@ -1,13 +1,69 @@
 const $ = (id) => document.getElementById(id)
 
+function saveData(data) {
+  chrome.storage.local.set(data, null)
+}
+
+function loadData(callback) {
+  chrome.storage.local.get(null, function (items) {
+    callback(items)
+  })
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+  loadData((data) => {
+    if (data.username) {
+      form.classList.add('hidden')
+
+      const user = $('username')
+      const rank = $('rank')
+      const level = $('level')
+      const leaderboardDiv = $('leaderboard')
+
+      const serverId = '461483989463597057'
+
+      const username = data.username
+      chrome.runtime.sendMessage(
+        { action: 'getUserData', serverId, username },
+        (response) => {
+          if (response && response.data) {
+            const { username, level: level_, rank: rank_ } = response.data
+            user.innerHTML = username
+
+            if (!rank_) {
+              rank.parentElement.remove()
+            } else {
+              rank.innerHTML = rank_
+            }
+
+            if (!level_) {
+              level.parentElement.remove()
+            } else {
+              level.innerHTML = level_
+            }
+          } else {
+            leaderboardDiv.innerHTML = 'Something went wrong.'
+          }
+
+          leaderboardDiv.classList.remove('hidden')
+        }
+      )
+    }
+  })
+})
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const form = e.target
+  const username = form.elements.username.value
+
   const user = $('username')
   const rank = $('rank')
   const level = $('level')
   const leaderboardDiv = $('leaderboard')
+  const formDiv = $('form')
 
   const serverId = '461483989463597057'
-  const username = 'lupcii'
 
   chrome.runtime.sendMessage(
     { action: 'getUserData', serverId, username },
@@ -30,6 +86,21 @@ document.addEventListener('DOMContentLoaded', () => {
       } else {
         leaderboardDiv.innerHTML = 'Something went wrong.'
       }
+
+      leaderboardDiv.classList.remove('hidden')
+      formDiv.classList.add('hidden')
+
+      saveData({ username })
     }
   )
+})
+
+$('change-username').addEventListener('click', () => {
+  const form = $('form')
+  const username = $('form-username')
+  const leaderboardDiv = $('leaderboard')
+
+  username.value = ''
+  form.classList.remove('hidden')
+  leaderboardDiv.classList.add('hidden')
 })
